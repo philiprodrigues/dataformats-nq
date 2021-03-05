a67b899a9dbafe622bc73c66549afd9d5b5aed9a
Philip Rodrigues <rodriges@fnal.gov>
2021-03-05 11:29:20 +0000

    Initial commit

---
+++ plugins/FragmentNQ.cpp
@@ -0,0 +1,5 @@
+#include "nwqueueadapters/AdapterMacros.hpp"
+#include "dataformats/Fragment.hpp"
+#include "dataformats-serde/FragmentSerDe.hpp"
+
+DEFINE_DUNE_NWQUEUEADAPTERS(std::unique_ptr<dunedaq::dataformats::Fragment>)
